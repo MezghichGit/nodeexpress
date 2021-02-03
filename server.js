8ce7d436b3ea763db763adbcbc2cbeb524b8e620
amine <amine.mezghich@ensi-uma.tn>
2021-02-03 12:28:55 +0100

    add de cors package

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
 const express = require('express');
 const app = express();
+const cors = require("cors");
+
 app.use(express.json());
 
+
+var corsOptions = {
+    //origin: "http://localhost:4200"
+    origin: "*"
+  };
+
+app.use(cors(corsOptions));
 
 // mettre le serveur à l'écoute(en marche) sur le port 85
 app.listen(
@@ -56,4 +65,28 @@ app.get('/equipes', (req,res) => {
                   throw err
               }
             })
-            
+    
+    app.put('/equipes/:id', async (req,res) => {
+                  try {
+                      const id = parseInt(req.params.id)
+                      const replacementEquipe = req.body
+                      const equipe = await db.collection('equipe').replaceOne({id},replacementEquipe)
+                      res.status(200).json(equipe)
+                  } catch (err) {
+                      console.log(err)
+                      throw err
+                  }
+                })
+    
+
+    app.delete('/equipes/:id', async (req,res) => {
+                      try {
+                          const id = parseInt(req.params.id)
+                          const equipe = await db.collection('equipe').deleteOne({id})
+                          res.status(200).json(equipe)
+                      } catch (err) {
+                          console.log(err)
+                          throw err
+                      } 
+                    })
+                    
